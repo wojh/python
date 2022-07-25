870c37844cfd7835821646f021460569a620c751
wojh <76084776+wojh@users.noreply.github.com>
2022-07-25 16:36:00 -0300

    Update multiplos_de_3.py

--- multiplos_de_3.py
+++ multiplos_de_3.py
@@ -2,8 +2,8 @@
 
 
 sum = int(0)
-for c in range(1, 501):
+for c in range(1, 501, 2):
     if c%3==0:
         sum += c
     
-print(f"A soma de todos múltiplos de 3 de 1 a 500 é: {sum}")
+print(f"A soma de todos múltiplos de 3 entre 1 e 500 é: {sum}")
