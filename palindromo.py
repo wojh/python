ff519522143b813128b8c3f73042b91dcc0817ed
wojh <76084776+wojh@users.noreply.github.com>
2022-07-25 15:15:50 -0300

    Update palindromo.py

--- palindromo.py
+++ palindromo.py
@@ -1,1 +1,12 @@
+#53 - Programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
 
+frase = input("Tecle a frase: ").strip()
+
+frase_clear = frase.replace(" ", "")
+
+esarf_clear = frase_clear[::-1]
+
+if esarf_clear == frase_clear:
+    print(f"SIM, '{frase}' é PALÍNDROMO")
+else:
+    print(f"NÃO, '{frase}' não é PALÍNDROMO")
