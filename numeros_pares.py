a97c846daa4b7e4462688391f49f8c6312a69769
wojh <76084776+wojh@users.noreply.github.com>
2022-07-25 16:31:51 -0300

    Update numeros_pares.py

--- numeros_pares.py
+++ numeros_pares.py
@@ -1,10 +1,6 @@
 # 47 - Programa que mostre na tela todos os números 
 # pares que estão no intervalo entre 1 e 50.
 
-
-print('ola')
-
-for c in range(1, 51):
-   if c%2==0:
-       print(c)
+for c in range(2, 51,2):
+   print(c, end=' ')
     
