a99443572e1e1ee1ff523c911b38772e976a3dd7
wojh <76084776+wojh@users.noreply.github.com>
2022-07-26 15:09:04 -0300

    Create 60-fatorial.py

---
+++ 60-fatorial.py
@@ -0,0 +1,25 @@
+# 60 - Faça um programa que leia um número qualquer
+# e mostre o seu fatorial. Com while e com for.
+
+from time import sleep
+
+num = int(input("Insira um número inteiro positivo: "))
+print('')
+
+print("Pronto!!! Em 2 segundos calcularemos seu fatorial!")
+print("")
+
+for c in range(1, 3)
+    print(c)
+    sleep(2)
+
+count = int(num - 1)
+prod = int(1)
+
+if num < 0:
+    print("Número Inválido!!! Precisa ser um número positivo!!!")
+elif num == 1 or num == 0:
+    print(f"Fatorial de {num} é 1")
+else:
+    while coun > 0:
+        prod 
