51f52b2182235923806a2094a71c6b8c1eaeef54
wojh <76084776+wojh@users.noreply.github.com>
2022-07-28 14:59:45 -0300

    Update 68.py

--- 68.py
+++ 68.py
@@ -1,11 +1,18 @@
 # 68 - Faça um program que jogue par ou ímpar com o computador. 
 # O jogo só será interrompido quando o jogador PERDER, mostrando 
 # o total de vitórias consecutivas que ele conquistou no final do jogo.
+
 from random import randint
+
 count = win = int(0)
+
 while True:
     n_user = int(input("Qual número escolhe ?? "))
     p_i = input("Par ou Ímpar [p/i]: ").lower()
+    
+    while (p_i != 'p' and p_i != 'i'):
+        p_i = input("Par ou Ímpar [p/i]: ").lower()
+    
     count += 1
     n_comp = int(randint(0, 2))
     
@@ -23,6 +30,7 @@ while True:
             print("Você perdeu!!!")
             print('')
             print('Fim de Jogo!!!')
+            print('')
             print(f"<<<Você jogou {count} vezes e ganhou {win} vezes>>> ")
             break
     else:
@@ -34,6 +42,9 @@ while True:
             print("Você perdeu!!!")
             print('')
             print("Fim de Jogo!!!")
+            print('')
             print(f"<<<Você jogou {count} vezes e ganhou {win} vezes>>>")
             break
     print('')
+    
+    
